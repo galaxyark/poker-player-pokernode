072f904afabcf49c61ca9c068163af79364b0f80
Fangzhou Lu <flu@newfieldwireless.com>
2016-07-21 11:36:21 -0700

    update

--- player.js
+++ player.js
@@ -4,7 +4,7 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    bet(game_state.current_buy_in);
+    bet(game_state.current_buy_in - game_state.players[game_state.in_action].bet);
   },
 
   showdown: function(game_state) {
