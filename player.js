c5d32fdeba878b4f2f6a6fdd2f10832163b3e5b3
Yun Zhou <yun.zhou@newfieldwireless.com>
2016-07-21 11:39:51 -0700

    change version name

--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 
 module.exports = {
 
-  VERSION: "Default JavaScript folding player",
+  VERSION: "aggressive pokerNode",
 
   bet_request: function(game_state, bet) {
     bet(game_state.current_buy_in - game_state.players[game_state.in_action].bet);
