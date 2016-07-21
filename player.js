daeaaa608fd9e4c5472234afac47d4052d23b685
mgordon <mgordon@alum.mit.edu>
2016-07-21 15:49:54 -0700

    Add 2-card ranking funciton

--- player.js
+++ player.js
@@ -99,3 +99,122 @@ module.exports = {
 
   }
 };
+
+// Return the size of the other stacks
+function otherStacks(game_state) {
+  var total = 0;
+  var count = game_state.players.length;
+  game_state.players.forEach(function(v, i) {
+    total += v.stack;
+  });
+  return total/count;
+}
+
+
+//x console.log(otherStacks(bet3.game_state));
+
+// Return 7 for pair of high cards
+// 5 for any other pair
+// 1-3 for single high cards or two of same suit
+function rank2cards(cards) {
+  function faceCard(card) {
+    return card.rank === "K" 
+      || card.rank === "Q"
+      || card.rank === "J"
+      || card.rank === "A";
+  }
+
+  if (cards.length < 2) {
+    return 0;
+  } else if (cards.length === 2) {
+    if (cards[0].rank == cards[1].rank) {
+      // a pair
+      if (faceCard(cards[0])) {
+        return 7;
+      } else {
+        return 5;
+      }
+    } else {
+      var score = 0
+      if ( cards[0].suit === cards[1].suit) {
+        score += 1;
+      }
+      if ( faceCard(cards[0]) || faceCard(cards[1]) ) {
+        score += 2;
+      }
+      if ( parseInt(cards[0].rank) >= 10) {
+        score += 1;
+      }
+      if ( parseInt(cards[1].rank) >= 10) {
+        score += 1;
+      }
+      return score;
+    }
+  }
+  return 0;
+}
+
+console.log(rank2cards([
+          { "rank": "K", "suit": "hearts"},
+          { "rank": "K", "suit": "spades"}
+    ])
+);
+
+
+// Return fraction being bet by most aggressive opponent
+function mostAggressive(game_state) {
+  var strongest = 0; // fraction
+  game_state.players.forEach(function(v, i) {
+    // Only look at other players, not us
+    if (!("hole_cards" in v) || v.hole_cards.length === 0) {
+      if (v.stack > 0) {
+        var f = v.bet/v.stack;
+        if (f > strongest) {
+          strongest = f;
+        }
+      }
+    }
+  })
+  return strongest;
+}
+
+var bets = {
+  "players": [
+      {
+        "id": 0,
+        "name": "Albert",
+        "status": "active",
+        "version": "Default random player",
+        "stack": 1010,
+        "bet": 320
+      },
+      {
+        "id": 1,
+        "name": "Bob",
+        "status": "active",
+        "version": "Default random player",
+        "stack": 1590,
+        "bet": 80,
+        "hole_cards": [
+          {
+            "rank": "6",
+            "suit": "hearts"
+          },
+          {
+            "rank": "K",
+            "suit": "spades"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Chuck",
+        "status": "out",
+        "version": "Default random player",
+        "stack": 0,
+        "bet": 0
+      }
+    ]
+};
+
+console.log("a=", mostAggressive(bets));
