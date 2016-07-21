e0ff7ebaabbd66f257dcace67792651d6dde84fd
Yang Zhang <zy31415@gmail.com>
2016-07-21 12:09:57 -0700

    Test get ranking.

--- player.js
+++ player.js
@@ -16,10 +16,16 @@ module.exports = {
     " pot= " + game_state.pot
     );
 
+    this.get_ranking();
+
     bet(game_state.current_buy_in - game_state.players[game_state.in_action].bet);
   },
 
   showdown: function(game_state) {
 
+  },
+
+  get_ranking: function() {
+    console.log("This function gets ranking.")
   }
 };
