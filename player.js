286de57bdfa1cb0c49fddb7261c3e80183459a9e
Fangzhou Lu <flu@newfieldwireless.com>
2016-07-21 14:27:35 -0700

    fix

--- player.js
+++ player.js
@@ -43,6 +43,7 @@ module.exports = {
     );
 
     this.getRanking(game_state, function() {
+      console.log('bet');
       bet(game_state.current_buy_in - game_state.players[game_state.in_action].bet);
     });
   },
