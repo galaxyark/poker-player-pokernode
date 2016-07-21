119628bf3973086a19a726413a13bcff9bf76733
mgordon <mgordon@alum.mit.edu>
2016-07-21 11:47:34 -0700

    add logging

--- player.js
+++ player.js
@@ -4,6 +4,18 @@ module.exports = {
   VERSION: "aggressive pokerNode",
 
   bet_request: function(game_state, bet) {
+
+    console.log(
+    "round= " + round,
+    " bet_index= " + bet_index,
+    " small_blind= " + small_blind,
+    " orbits= " + orbits,
+    " dealer= " + dealer,
+    " community_cards= " + community_cards,
+    " current_buy_in= " + current_buy_in,
+    " pot= " + pot
+    );
+
     bet(game_state.current_buy_in - game_state.players[game_state.in_action].bet);
   },
 
