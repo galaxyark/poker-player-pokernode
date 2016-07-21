aa8ca2a0500c32c631470cc854e750a19a08b5cf
Fangzhou Lu <flu@newfieldwireless.com>
2016-07-21 13:46:00 -0700

    Get ranking.

--- player.js
+++ player.js
@@ -1,54 +1,45 @@
-
 module.exports = {
-
   VERSION: "aggressive pokerNode",
 
+  holdCards: function(game_state) {
+    return game_state.players[game_state.in_action].hold_cards;
+  },
+
+  ranking: [],
+
+  getRanking: function(game_state, callback) {
+    var cards = this.holdCards(game_state).concat(game_state.community_cards);
+    var request = require('request');
+    var self = this;
+
+    request.get({
+      headers: {'content-type' : 'application/x-www-form-urlencoded'},
+      url:     'http://rainman.leanpoker.org/rank',
+      body:    "cards=" + JSON.stringify(cards)
+    }, function(error, response, body) {
+      self.ranking = body;
+      console.log(body);
+    });
+  },
+
   bet_request: function(game_state, bet) {
+    console.log(
+    "round= " + round,
+    " bet_index= " + game_state.bet_index,
+    " small_blind= " + game_state.small_blind,
+    " orbits= " + game_state.orbits,
+    " dealer= " + game_state.dealer,
+    " community_cards= " + game_state.community_cards,
+    " current_buy_in= " + game_state.current_buy_in,
+    " pot= " + game_state.pot
+    );
 
-    this.output_gamestate(game_state);
-
-    this.get_ranking();
-
-    bet(game_state.current_buy_in - game_state.players[game_state.in_action].bet);
+    this.getRanking(game_state, function() {
+      bet(game_state.current_buy_in - game_state.players[game_state.in_action].bet);
+    });
   },
 
   showdown: function(game_state) {
 
-  },
-
-  //////////////////////
-  // PokerNode defined functions:
-
-  // Fang Zhou
-  get_ranking: function() {
-    // Get the ranking of the cards
-    console.log("This function gets ranking.");
-
-  },
-
-  // Micheal
-  output_gamestate: function(game_state) {
-    console.log(
-        "round= " + game_state.round,
-        " bet_index= " + game_state.bet_index,
-        " small_blind= " + game_state.small_blind,
-        " orbits= " + game_state.orbits,
-        " dealer= " + game_state.dealer,
-        " community_cards= " + game_state.community_cards,
-        " current_buy_in= " + game_state.current_buy_in,
-        " pot= " + game_state.pot
-    );
-  },
-
-  //Yun:
-  next_move: function(){
-
-  },
-
-
-  //Yang
-  bet_size: function(game_state, rank){
-
   }
-
 };
